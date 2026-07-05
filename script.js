09b734f225b626e251319551d75aa4c1b7d90f19
Isse Mohamed Bulale <isse4935391@gmail.com>
2026-07-05 08:16:33 +0300

    Learn JS variables and operators

---
+++ script.js
@@ -0,0 +1,36 @@
+console.log("Hello from JavaScript");
+
+let age = 25;
+const name="Isse";
+ 
+age = 28;
+
+let title = "Web Development";
+let year = 2026;
+let isLearning = true;
+
+console.log(typeof title);
+console.log(typeof year);
+
+console.log(name + " is " + age + " years old and is learning " + title + " in the year " + year );
+
+console.log(10 + 5);
+console.log(10 / 2);
+console.log(10 * 3);
+console.log(10 % 3);
+
+let firstName = "Isse";
+let lastName = "Bulale";
+let fullname = firstName + " " + "Mohamed" + " " + lastName;
+
+console.log(fullname);
+
+
+const name1 = "isse";
+let age1 = 28;
+console.log("my name is " + name1 + " and I am " + age1 + " years old");
+console.log(age1 * 10);
+console.log(typeof name1);
+console.log(typeof age1);
+console.log(age1 > 18);
+
